a764021b51a9efff0a1657af5fda0038a700d524
Lukhsaan <elankuml@mcmaster.ca>
2024-11-25 21:26:44 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include "ram_reading.h"
 #include "readsystem.h"
 
-int main(int argc, char *argv[])
-{
-	if (argc == 2 && (strcmp(argv[1], "--help") == 0))
-	{
-		 char help[] =
-        	"The --help command\n"
-        	"Usage: read \n\n"
-        	"Outputs CPU usage of top 5 apps and RAM usage\n\n"
-       	 	"--help\tcurrent text\n\n"
-        	"Examples: \n"
-        	"read\t outputs all the CPU and RAM stats\n";
-        	printf("%s", help);	
-	}
-	
-	else
-	{
-		printMemoryInGB();
-		get_top_cpu_processes();
-	}
+int main(int argc, char *argv[]) {
+    if (argc == 2 && strcmp(argv[1], "--help") == 0) {
+        char help[] =
+            "The --help command\n"
+            "Usage: readsystem\n\n"
+            "Outputs CPU usage of top 5 apps and RAM usage\n\n"
+            "--help\tDisplays this text\n\n"
+            "Examples: \n"
+            "readsystem\tOutputs all the CPU and RAM stats\n";
+        printf("%s", help);
+    } else {
+        get_top_cpu_processes();
+    }
+    return 0;
 }
