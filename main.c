bbfe1f74d33a51fa35546668888c86455817c05a
Lukhsaan <elankuml@mcmaster.ca>
2024-12-03 20:25:25 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,18 @@
+#include "readsystem.h"
 #include <stdio.h>
 #include <string.h>
-#include "readsystem.h"
 
 int main(int argc, char *argv[]) {
     if (argc == 2 && strcmp(argv[1], "--help") == 0) {
         char help[] =
-            "The --help command\n"
-            "Usage: readsystem\n\n"
-            "Outputs CPU usage of top 5 apps and RAM usage\n\n"
-            "--help\tDisplays this text\n\n"
-            "Examples: \n"
-            "readsystem\tOutputs all the CPU and RAM stats\n";
+            "Usage: readsystem\n"
+            "Outputs CPU usage of top 5 apps and RAM usage.\n"
+            "--help: Displays this text.\n";
         printf("%s", help);
     } else {
-        get_top_cpu_processes();
+        get_top_cpu_processes(); 
+        print_battery_status();  
     }
     return 0;
 }
+
